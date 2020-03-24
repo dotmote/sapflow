9e53a9723185fcfe2f1c33285725f6b3c68f2b89
Kevin Xu <khxu@berkeley.edu>
2020-03-23 21:41:12 -0700

    initial commit of mqtt_web_server

---
+++ software/edge_compute_node/raspberry_pi/mqtt_web_server_nodejs/index.js
@@ -0,0 +1,124 @@
+const mqtt = require('mqtt');
+const fs = require('fs');
+const csv = require('fast-csv');
+
+const HOST = 'mqtt://localhost'; // IP address (e.g., 192.168.1.2) of the MQTT broker (in this case, the IP address of the raspberry pi itself).
+const TOPIC = 'sapflow';
+
+const client = mqtt.connect(HOST, {
+	// Config fields if needed to connect to the MQTT broker
+	// username: 'username',
+	// password: 'password',
+	clientId: 'clientName'
+});
+
+client.on('connect', () => {
+	client.subscribe(TOPIC);
+});
+
+client.on('message', (topic, message) => {
+	try {
+		const messageJSON = JSON.parse(message.toString());
+		console.log(`MQTT message received: ${JSON.stringify(messageJSON)}`);
+		const {
+			id,
+			temp1,
+			temp2,
+			millisSinceHeatPulse,
+			outsideTemp,
+			outsideHumidity,
+			soilMoisture,
+			rtcUnixTimestamp,
+			millisSinceReferenceTemp
+		} = messageJSON;
+		console.log('nodeID: ', id);
+		console.log('temp1', temp1);
+		console.log('temp2', temp2);
+		console.log('millisSinceHeatPulse', millisSinceHeatPulse);
+
+		const currentTime = new Date();
+
+		const currentTimeRoundedToHour =
+			Date.UTC(
+				currentTime.getUTCFullYear(),
+				currentTime.getUTCMonth(),
+				currentTime.getUTCDate(),
+				currentTime.getUTCHours()
+			) / 1000;
+
+		const unixTimestamp = Math.floor(Date.now() / 1000);
+
+		const humanTimestamp = `${currentTime.getUTCMonth() +
+			1}/${currentTime.getUTCDate()}/${currentTime.getUTCFullYear()} ${currentTime.getUTCHours()}:${currentTime.getUTCMinutes()}:${currentTime.getUTCSeconds()}`;
+
+		// human readable string for datafiles. format is:
+		// <nodeId>_<localTimeRoundedToHour>_<unixTimestamp>.csv
+		// for example:
+		// 2_2_2020-02-17T21_00_00.000Z_1581973200.csv
+		const localTimeRoundedToHour = new Date(
+			currentTime.getFullYear(),
+			currentTime.getMonth(),
+			currentTime.getDate(),
+			currentTime.getHours()
+		).toISOString().replace(/:/g, '_');
+
+		console.log('Writing to csv...');
+
+		const wsHeader = fs.createWriteStream(`./data/${id}_${localTimeRoundedToHour}_${currentTimeRoundedToHour}.csv`, { flags: 'wx' });
+		wsHeader.on('error', (err) => {
+			console.log('Header row written. Appending data to existing file.');
+		});
+
+		const wsRow = fs.createWriteStream(`./data/${id}_${localTimeRoundedToHour}_${currentTimeRoundedToHour}.csv`, { flags: 'a' });
+
+		// write header row to top of CSV
+		csv.writeToStream(
+			wsHeader,
+			[
+				[
+					'unix_timestamp',
+					'human_timestamp',
+					'nodeID',
+					'temp1',
+					'temp2',
+					'millisSinceHeatPulse',
+					'outsideTemp',
+					'outsideHumidity',
+					'soilMoisture',
+					'rtcUnixTimestamp',
+					'millisSinceReferenceTemp'
+				]
+			],
+			{ headers: false }
+		);
+
+		// append data to existing CSV
+		csv.writeToStream(
+			wsRow,
+			[
+				// [ 'unix_timestamp', 'human_timestamp', 'nodeID', 'temp1', 'temp2', 'millisSinceHeatPulse' ],
+				[''],
+				[
+					unixTimestamp,
+					humanTimestamp,
+					id,
+					temp1,
+					temp2,
+					millisSinceHeatPulse,
+					outsideTemp,
+					outsideHumidity,
+					soilMoisture,
+					rtcUnixTimestamp,
+					millisSinceReferenceTemp
+				]
+			],
+			{ headers: false }
+		);
+	} catch (e) {
+		if (e instanceof SyntaxError) {
+			console.log(`Syntax error in MQTT message: ${e.message}`);
+		} else {
+			console.log(`Error: ${e}`);
+		}
+	}
+});
