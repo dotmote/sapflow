9cd7761856f03a249080e832d2cea6623a6ee3de
Kevin Xu <khxu@berkeley.edu>
2020-05-05 20:47:04 -0700

    track build directory

---
+++ software/edge_compute_node/raspberry_pi/mqtt_web_server_nodejs/frontend/build/precache-manifest.232feeeaf6c3fd7b1b89450365250fd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ee38176aa22d1cd353f01e85c1a043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "799d43eb078baa99da57",
+    "url": "/static/css/main.1487bcf6.chunk.css"
+  },
+  {
+    "revision": "a9926ce78af5cd67de6b",
+    "url": "/static/js/2.a23c0035.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a23c0035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "799d43eb078baa99da57",
+    "url": "/static/js/main.a4f1d468.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "68d2b4f99fc333ded2d6b4dd688acfce",
+    "url": "/static/media/logo.68d2b4f9.png"
+  }
+]);
